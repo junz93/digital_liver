d432a935c0d769ae5f46e33c4d82959c6b68afc5
Jun Zhang <junz93@outlook.com>
2023-09-09 14:11:15 +0800

    优化流式接口的用户认证

--- apps/material/consumers.py
+++ apps/material/consumers.py
@@ -1,12 +1,10 @@
-import asyncio
 import json
 import logging
 import threading
 import time
 
 # from asgiref.sync import sync_to_async
-from channels.db import database_sync_to_async
-# from channels.generic.http import AsyncHttpConsumer
+# from channels.db import database_sync_to_async
 from channels.generic.websocket import JsonWebsocketConsumer
 from urllib.parse import parse_qs
 
@@ -27,9 +25,6 @@ class AiGeneratorWsConsumer(CustomJsonWebsocketConsumer):
         self.generating = False
         
         # logging.info(f'Websocket scope: {self.scope}')
-
-        self.accept()
-        logging.info('Websocket connection opened')
         
         path_params = self.scope['url_route']['kwargs']
         query_params = parse_qs(self.scope['query_string'].decode(encoding='utf-8'))
@@ -40,6 +35,9 @@ class AiGeneratorWsConsumer(CustomJsonWebsocketConsumer):
             or (self.mode != gpt.AnswerMode.CHAT and self.mode != gpt.AnswerMode.SCRIPT):
             self.close()
             return
+        
+        self.accept()
+        logging.info('Websocket connection opened')
         
         self.query = query_params['query'][0]
         
@@ -89,89 +87,11 @@ class AiGeneratorWsConsumer(CustomJsonWebsocketConsumer):
             self.close()
     
     def _monitor(self):
-        time.sleep(5)
+        for _ in range(5):
+            if self.generating:
+                return
+            time.sleep(1)
+        
         if not self.generating:
             self.close()
             logging.info(f'No client message in 5 seconds. Closing the connection...')
-
-
-# class AiGeneratorConsumer(AsyncHttpConsumer):
-#     async def handle(self, body):
-#         await self.send_headers(headers=[
-#             (b'Cache-Control', b'no-cache'),
-#             (b'Content-Type', b'text/event-stream; charset=utf-8'),
-#             # (b'Transfer-Encoding', b'chunked'),
-#         ])
-
-#         user = self.scope['user']
-#         if not user.is_authenticated:
-#             await self.close(403, 'Not logged in')
-#             return
-
-#         # if await database_sync_to_async(is_usage_limit_reached)(user):
-#         #     await self.close(403, 'Max usage time was reached')
-#         #     return
-
-#         path_params = self.scope['url_route']['kwargs']
-#         query_params = parse_qs(self.scope['query_string'].decode(encoding='utf-8'))
-#         mode = path_params['mode'].upper()
-
-#         if 'character_id' not in path_params \
-#             or 'query' not in query_params \
-#             or (mode != gpt.AnswerMode.CHAT and mode != gpt.AnswerMode.SCRIPT):
-#             await self.close(400, 'Invalid input')
-#             return
-        
-#         character_id = int(path_params['character_id'])
-#         query = query_params['query'][0]
-
-#         try:
-#             character = await self.get_character(character_id, user.id)
-#             # for segment in await sync_to_async(gpt.get_answer)(
-#             for segment in gpt.get_answer(
-#                 query, 
-#                 f'user_{user.id}' if mode == gpt.AnswerMode.CHAT else None, 
-#                 int(time.time()), 
-#                 with_censorship=False, 
-#                 character=character, 
-#                 mode=mode
-#             ):
-#                 if segment.startswith('生成Gpt回答出错'):
-#                     await self.close(500, 'Failed to generate content')
-#                     return
-                
-#                 # logging.info('Received a segment')
-#                 await self.send_message(segment)
-#                 await asyncio.sleep(0.1)
-            
-#             await self.close(200, 'Completed successfully')
-#         except Character.DoesNotExist:
-#             await self.close(404, f'Cannot find a character with ID {character_id}')
-#         except Exception as e:
-#             logging.exception('An error happened while generating content')
-#             await self.close(500, 'An error happened while generating content')
-
-#     @database_sync_to_async
-#     def get_character(self, character_id: int, user_id: int):
-#         return Character.objects.get(id=character_id, user_id=user_id)
-
-#     async def send_message(
-#         self, 
-#         data, 
-#         code: int = None, 
-#         message: str = None, 
-#         is_last = False
-#     ):
-#         json_data = { 'data': data }
-#         if code is not None:
-#             json_data['code'] = code
-#         if message is not None:
-#             json_data['message'] = message
-
-#         data = json.dumps(json_data, ensure_ascii=False)
-
-#         body = f'data: {data}\n\n'.encode('utf-8')
-#         await self.send_body(body, more_body=(not is_last))
-    
-#     async def close(self, code: int, message: str):
-#         await self.send_message('', code, message, True)
